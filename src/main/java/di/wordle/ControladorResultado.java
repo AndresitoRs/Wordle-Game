0aac1583e57934d07afcc7e2bd2730aeea92a051
Andrés <andres.rivada@iessanmamede.com>
2025-11-06 09:38:39 +0100

    Creación de la carpeta de imagenes en la que se usan diferentes de ellas para la creación del fxml del menú

--- src/main/java/di/wordle/ControladorResultado.java
+++ src/main/java/di/wordle/ControladorResultado.java
@@ -1,4 +1,17 @@
 package di.wordle;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 public class ControladorResultado {
+
+    @FXML
+    Resultado info;
+    @FXML
+    ImageView imagen;
+
+    public ImageView getImagen(){
+        return imagen;
+    }
 }
